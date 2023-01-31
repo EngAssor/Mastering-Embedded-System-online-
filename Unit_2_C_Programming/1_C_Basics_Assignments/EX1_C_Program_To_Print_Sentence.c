04ad50acbc9b816e8b10abfc08c881ce57c69ecc
Eng Asser <Aserfs2000@gmail.com>
2023-01-31 04:06:55 +0200

    Ex1

---
+++ Unit_2_C_Programming/1_C_Basics_Assignments/EX1_C_Program_To_Print_Sentence.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : EX1_C_Program_To_Print_Sentence.c
+ Author      : Asser Tantawy
+ Version     : 0.0
+ Copyright   : Your copyright notice
+ Description : this basic C programming exercises
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+printf("C Programming");
+}
