c9deb4239dab878d83826a0bc84b581a62f6f29c
Asser Tantawy <Aserfs2000@gmail.com>
2023-02-21 16:44:07 +0200

    functions Ex

--- Unit_2_C_Programming/4_Functions/Homework/EX2_C_Program_to_Calculate_Factorial_of_a_Number_Using_Recursion..c
+++ Unit_2_C_Programming/4_Functions/Homework/EX2_C_Program_to_Calculate_Factorial_of_a_Number_Using_Recursion..c
@@ -10,19 +10,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int Get_Fact(int x);
+int Get_Fact(int x); // function prototype
 int main(void) {
-	int num;
-	printf("Enter a positive Intger :");
-	scanf("%d",&num);
-	printf("factorial of %d = %d",num,Get_Fact(num));
+	int num; // the number to get factorial
+	printf("Enter a positive Intger :");// message to user
+	scanf("%d",&num); // scanning the number
+	printf("factorial of %d = %d",num,Get_Fact(num));// printing the factorial
 	return 0;
 }
-int Get_Fact(int x)
+int Get_Fact(int x) // function body
 {
 	int fact=x;
-	if(x==1)
+	if(x==1) // checking the end of recursion
 		return 1;
-	return fact * Get_Fact(--x);
+	return fact * Get_Fact(--x);// using recursion to get factorial
 }
 
